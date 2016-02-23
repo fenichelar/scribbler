1d209a57600d5b5cb765b81dd1703a61f3a39d66
Alec Fenichel <alec.fenichel@gmail.com>
2016-02-23 15:07:39 -0500

    Add lab1

--- lab1.py
+++ lab1.py
@@ -1,3 +1,54 @@
+#!/usr/local/bin/python2
+
 from myro import *
 
 init("/dev/tty.scribbler")
+
+def getDirection(direction):
+    if direction is 'left':
+        return 1
+    elif direction is 'right':
+        return -1
+    else:
+        raise NameError('Invalid direction!')
+
+def obstableDetected():
+    return (max(getObstacle()) > 1500)
+
+def moveForward():
+    translate(1)
+
+def moveForwardUntilWall():
+    moveForward()
+    while not obstableDetected():
+        pass
+    stop()
+
+def rotate90Degrees(direction):
+    stop()
+    rotate(direction)
+    wait(.63)
+    stop()
+
+def checkForWall(direction):
+    stop()
+    rotate90Degrees(-direction)
+    obstable = obstableDetected()
+    rotate90Degrees(direction)
+    return obstable
+
+
+wallTurnDirection = getDirection('right')
+moveForwardUntilWall()
+rotate90Degrees(wallTurnDirection)
+moveForward()
+counter = 0
+while not obstableDetected():
+    counter+=1
+    if counter > 10:
+        if checkForWall(wallTurnDirection):
+            moveForward()
+            counter = 0
+        else:
+            break
+stop()
